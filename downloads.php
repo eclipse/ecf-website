1fd9598b3df4507e21e42a8b55540c45c5870183
slewis <slewis>
2006-11-15 20:56:16 +0000

    updated downloads page with uninstall instructions

--- downloads.php
+++ downloads.php
@@ -34,7 +34,7 @@ ob_start();
 	<div id="midcolumn">
 	   <div class="homeitem3col">
 		 <h3><?= $pageTitle ?></h3>
-         <p><img border="0" src="../images/new.gif" width="31" height="14"><b>Eclipse Communication Framework 0.9.2</b> &quot;Stable&quot; build is now available.</p>
+         <p><img border="0" src="../images/new.gif" width="31" height="14"><b>Eclipse Communication Framework 0.9.3</b> &quot;Stable&quot; build is now available.</p>
          <p><h4>IMPORTANT:  The 0.9.3 release is a <b>compatibility-breaking release</b>, meaning that</h4>
                <ul>
                   <li>If you are using a previous version of ECF (<= 0.9.2), it is necessary to <b>uninstall</b> this
