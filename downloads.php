8331355d0e0e10b8627c6d6e37a6e13dbaaf921d
pmackie <pmackie>
2007-01-13 23:01:18 +0000

    *** empty log message ***

--- downloads.php
+++ downloads.php
@@ -42,7 +42,7 @@ ob_start();
 	<div id="midcolumn">
 	   <div class="homeitem3col">
 		 <h3><?= $pageTitle ?></h3>
-         <p><img border="0" src="../images/new.gif" width="31" height="14"><b>Eclipse Communication Framework 0.9.5</b> Stable build is now available.</p>
+         <p><img border="0" src="../images/new.gif" width="31" height="14"><b>Eclipse Communication Framework 0.9.6</b> Stable build is now available.</p>
          <p><h4><img border="0" src="../images/new.gif" width="31" height="14">IMPORTANT:  The 0.9.3, and newer, releases are a <b>compatibility-breaking release</b>, meaning that:</h4>
                <ul>
                   <li>If you are using a previous version of ECF (<= 0.9.2), it is necessary to <b>uninstall</b> this
