06375cb5b0bc80faa1192050ff642ae61b18b7b5
slewis <slewis>
2007-02-26 21:31:25 +0000

    Changed events listing on ecf home page

--- index.php
+++ index.php
@@ -63,10 +63,16 @@
 
 	<tr>
 		<td align="left" valign="top" height="12" width="23">
+			<a href="http://eclipsecon.org/2007"><img border=0 src="images/logo-eclipsecon.gif"></a>
+		</td>
+	</tr>
+	
+	<tr>
+		<td align="left" valign="top" height="12" width="23">
 			<img src="../images/Adarrow.gif" border="0" width="16" height="16" alt="[Adarrow]"/>
 		</td>
 		<td valign="top">
-	  	<b>February 15, 2007 - ECF 1.0.0M5-stable build released on eclipse.org. </b>
+	  	<b>February 15, 2007 - ECF 1.0.1-stable build released on eclipse.org. </b>
 			See <a href="NewAndNoteworthy.html">New and Noteworthy</a> release documentation.
 		</td>
 	</tr>
@@ -79,81 +85,6 @@
 	  	<b>January 13, 2007 - ECF 0.9.6-stable build released on eclipse.org. </b>  
 		</td>
 	</tr>
-	<tr>
-		<td align="left" valign="top" height="12" width="23">
-			<img src="../images/Adarrow.gif" border="0" width="16" height="16" alt="[Adarrow]"/>
-		</td>
-		<td valign="top">
-	  	<b>December 22, 2006 - ECF 0.9.5-stable build released on eclipse.org. </b>
-		</td>
-	</tr>
-
-       <tr>
-         <td align="left" valign="top" height="12" width="23">
-          <img src="../images/Adarrow.gif" border="0" width="16" height="16" alt="[Adarrow]"/>
-         </td>
-         <td valign="top">
-	  <b>December 2, 2006 - ECF 0.9.4-stable build released on eclipse.org. </b>
-	 </td>
-       </tr>
-
-       <tr>
-         <td align="left" valign="top" height="12" width="23">
-          <img src="../images/Adarrow.gif" border="0" width="16" height="16" alt="[Adarrow]"/>
-         </td>
-         <td valign="top">
-	  <b>November 15, 2006 - ECF 0.9.3-stable build released on eclipse.org. </b>
-            
-  		NOTE: 0.9.3 is a compatibility-breaking release, with major API changes and improvements.  
-          If you are using 0.9.2 or previous versions it will 
-	  be necessary to recompile applications.
-	 </td>
-       </tr>
-
-       <tr>
-	<td align="left" valign="top" height="12" width="23">
-	 <img src="../images/Adarrow.gif" border="0" width="16" height="16" alt="[Adarrow]"/>
-	</td>
-	<td valign="top">
-	 <b>October 6, 2006 - ECF 0.9.2-stable build released on eclipse.org.</b>
-        </td>
-       </tr>
-
-       <tr>
-         <td align="left" valign="top" height="12" width="23">
-	  <img src="../images/Adarrow.gif" border="0" width="16" height="16" alt="[Adarrow]"/>
-	 </td>
-         <td valign="top">
-          <b>September 6, 2006 - ECF 0.9.1-stable build released on eclipse.org.</b>
-         </td>
-       </tr>
-
-       <tr>
-        <td align="left" valign="top" height="12" width="23">
-         <img src="../images/Adarrow.gif" border="0" width="16" height="16" alt="[Adarrow]"/>
-        </td>
-        <td valign="top">
-         <b>August 17, 2006 - ECF 0.9.0-stable build released on eclipse.org.</b>
-        </td>
-       </tr>
-
-       <tr>
-        <td align="left" valign="top" height="12" width="23">
-         <img src="../images/Adarrow.gif" border="0" width="16" height="16" alt="[Adarrow]"/>
-	</td>
-	<td valign="top">
-         <b>June 24, 2006 - ECF 0.8.9-stable build released on eclipse.org.</b>
-	</td>
-       </tr>
-
-       <tr>
-	<td align="left" valign="top" height="12" width="23">
-         <img src="../images/Adarrow.gif" border="0" width="16" height="16" alt="[Adarrow]"/>
-        </td>
-        <td valign="top">
-         <b>June 27, 2006 - ECF 0.8.7-stable build released on eclipse.org.</b>
-	</td>
-       </tr>
 
         <tr>
          <td align="left" valign="top" height="12" width="23">
@@ -170,72 +101,9 @@
           <img src="../images/Adarrow.gif" border="0" width="16" height="16" alt="[Adarrow]"/>
          </td>
          <td valign="top">
-          <b>June 19, 2006 - ECF 0.8.6-stable build</b>
-         </td>
-        </tr>
-
-        <tr>
-         <td align="left" valign="top" height="12" width="23">
-          <img src="../images/Adarrow.gif" border="0" width="16" height="16" alt="[Adarrow]"/>
-         </td>
-         <td valign="top">
-          <b>May 28, 2006 - ECF 0.8.3-stable build</b>
-         </td>
-        </tr>
-
-        <tr>
-         <td align="left" valign="top" height="12" width="23">
-          <img src="../images/Adarrow.gif" border="0" width="16" height="16" alt="[Adarrow]"/>
-         </td>
-         <td valign="top">
-          <b>May 23, 2006 - ECF 0.8.2-stable build</b>
-         </td>
-        </tr>
-
-        <tr>
-         <td align="left" valign="top" height="12" width="23">
-          <img src="../images/Adarrow.gif" border="0" width="16" height="16" alt="[Adarrow]"/>
-         </td>
-         <td valign="top">
-          <b>May 14, 2006 - ECF 0.8.1-stable build</b>
-         </td>
-        </tr>
-
-        <tr>
-         <td align="left" valign="top" height="12" width="23">
-          <img src="../images/Adarrow.gif" border="0" width="16" height="16" alt="[Adarrow]"/>
-         </td>
-         <td valign="top">
-          <b>May 7, 2006 - ECF 0.8.0-stable build</b>
-         </td>
-        </tr>
-
-        <tr>
-          <td align="left" valign="top" height="12" width="23">
-          <img src="../images/Adarrow.gif" border="0" width="16" height="16" alt="[Adarrow]"/>
-         </td>
-         <td valign="top">
-          <b>April 13, 2006 - ECF 0.7.7-stable build</b>
-         </td>
-        </tr>
-
-        <tr>
-         <td align="left" valign="top" height="12" width="23">
-          <img src="../images/Adarrow.gif" border="0" width="16" height="16" alt="[Adarrow]"/>
-         </td>
-         <td valign="top">
           <b>April 12, 2006 - New tutorial documentation for running the ECF 'scribbleshare' example
           whiteboard application.</b>  
           See <a href="scribbleshare.html"/><b>here</b>
-         </td>
-        </tr>
-
-        <tr>
-         <td align="left" valign="top" height="12" width="23">
-          <img src="../images/Adarrow.gif" border="0" width="16" height="16" alt="[Adarrow]"/>
-         </td>
-         <td valign="top">
-          <b>April 1, 2006 - ECF 0.7.6-stable build</b>
          </td>
         </tr>
 
@@ -269,15 +137,6 @@
            <b>March 14, 2006 - IBM developerWorks® Article: </b>
            <a href="http://www-128.ibm.com/developerworks/opensource/library/os-ecl-commfwk/">
            Getting started with the Eclipse Communication Framework</a>
-          </td>
-         </tr>
-
-         <tr>
-          <td align="left" valign="top" height="12" width="23">
-           <img src="../images/Adarrow.gif" border="0" width="16" height="16" alt="[Adarrow]"/>
-          </td>
-          <td valign="top">
-           <b>November 1, 2005 - ECF 0.5.0-stable build</b>
           </td>
          </tr>
 
