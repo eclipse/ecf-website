9c84282a9fac41f423414015b27bf19636d87c25
slewis <slewis>
2007-05-11 21:46:59 +0000

    Changes to download page

--- downloads.php
+++ downloads.php
@@ -41,44 +41,8 @@ ob_start();
 
 
     <p><img border="0" src="../images/new.gif" width="31" height="14" alt="[new]"/>
-     <b>Eclipse Communication Framework 1.0.0 milestone 6 build is now available.</b>
+     <b>Eclipse Communication Framework 1.0.0 milestone 7 build is now available.</b>
     <p></p>
-
-    <h4><img border="0" src="../images/new.gif" width="31" height="14" alt="[new]"/>
-     <b>IMPORTANT:  The 0.9.3, and newer, releases are a compatibility-breaking release</b>, meaning that:
-    </h4>
-
-    <ul>
-     <li>If you are using a previous version of ECF (&gt;&#61; 0.9.2), it is necessary to <b>uninstall</b> this
-     version <b>before</b> installing 0.9.6.  To uninstall the previous version:
-
-      <ul>
-       <li>In Eclipse, go to <b>Help -> Software Updates -> Manage Configuration</b>
-       </li>
-       <li>Select all the features that start with "ECF ...".
-       </li>
-       <li>Right click to bring up context menu and choose <b>Uninstall</b>
-       </li>
-       <li>Answer Yes/OK to questions
-       </li>
-       <li>Stop and restart Eclipse
-       </li>
-       <li>
-       Now you may install 0.9.6 (via update site or zip).  See below for instructions.
-       </li>
-      </ul>
-     </li>
-     <li>Applications based upon 0.9.6 will not work with older servers (&gt;&#61; 0.9.2).  
-     </li>
-     <li>Applications created with ECF releases previous to 0.9.3 will not work with new servers.  
-     The public ECF server at 
-     <b>ecftcp://ecf.eclipse.org:3282/server has moved to 0.9.6. The ECF server no longer support old clients (0.9.2).</b>
-     </li>
-    </ul>
-
-    <h4><b>Please see <a href="NewAndNoteworthy_0.9.6.html">New and Noteworthy for 0.9.6</a> for details about API changes</b></h4>
-    <p></p>
-
 
 <?php
 	$html = ob_get_contents();
