9f3f2868d65fb596728ffc821c6525a00c778de2
slewis <slewis>
2007-06-27 17:58:43 +0000

    website updates for latest build

--- index.php
+++ index.php
@@ -65,6 +65,15 @@
 			<img src="../images/Adarrow.gif" border="0" width="16" height="16" alt="[Adarrow]"/>
 		</td>
 		<td valign="top">
+	  	<b>June 27, 2007 - ECF 1.0.0 released on eclipse.org. </b>
+			See <a href="downloads.php">here for download</a>.  See <a href="NewAndNoteworthy.html">New and Noteworthy</a> release documentation.
+		</td>
+	</tr>
+	<tr>
+		<td align="left" valign="top" height="12" width="23">
+			<img src="../images/Adarrow.gif" border="0" width="16" height="16" alt="[Adarrow]"/>
+		</td>
+		<td valign="top">
 	  	<b>June 22, 2007 - <a href="http://villane.blogspot.com/2007/06/equinox-ecf-serving-interactive-fiction.html">Really cool:  Server-side interactive fiction done with Equinox and ECF</a>. </b>
 		</td>
 	</tr>
@@ -73,35 +82,7 @@
 			<img src="../images/Adarrow.gif" border="0" width="16" height="16" alt="[Adarrow]"/>
 		</td>
 		<td valign="top">
-	  	<b>June 20, 2007 - ECF 1.0.0 RC4+1 released on eclipse.org. </b>
-			See <a href="downloads.php">here for download</a>.  See <a href="NewAndNoteworthy.html">New and Noteworthy</a> release documentation.
-		</td>
-	</tr>
-	<tr>
-		<td align="left" valign="top" height="12" width="23">
-			<img src="../images/Adarrow.gif" border="0" width="16" height="16" alt="[Adarrow]"/>
-		</td>
-		<td valign="top">
 	  	<b>May 14, 2007 - <a href="http://www.eclipsezone.com/eclipse/forums/t94530.html">Podcast with ECF project lead Scott Lewis.</a></b>
-		</td>
-	</tr>
-	<tr>
-		<td align="left" valign="top" height="12" width="23">
-			<img src="../images/Adarrow.gif" border="0" width="16" height="16" alt="[Adarrow]"/>
-		</td>
-		<td valign="top">
-	  	<b>May 11, 2007 - ECF 1.0.0 M7 released on eclipse.org. </b>
-			See <a href="downloads.php">here for download</a>.  See <a href="NewAndNoteworthy.html">New and Noteworthy</a> release documentation.
-		</td>
-	</tr>
-	<tr>
-		<td align="left" valign="top" height="12" width="23">
-			<img src="../images/Adarrow.gif" border="0" width="16" height="16" alt="[Adarrow]"/>
-		</td>
-		<td valign="top">
-	  	<b>April 28, 2007 - ECF 1.0.0 dev build released on eclipse.org. </b>
-			See <a href="http://dev.eclipse.org/mhonarc/lists/ecf-dev/msg00786.html">Mailing List/Newsgroup Posting</a>.  Note this
-			includes a new <a href="http://wiki.eclipse.org/index.php/Skype_Provider">Skype Provider for ECF</a>.
 		</td>
 	</tr>
 	<tr>
