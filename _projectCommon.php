45c83cd281f0415730c81251b7a8f9d601f2b1bf
tkubaska <tkubaska>
2008-11-11 18:33:28 +0000

    Added more custom navs and tried out local references

--- _projectCommon.php
+++ _projectCommon.php
@@ -23,17 +23,20 @@
  	$Nav->addNavSeparator("ECF Home", "/ecf/");
     $Nav->addCustomNav("About This Project","http://www.eclipse.org/projects/project_summary.php?projectid=rt.ecf","_self",1);
     
-    $Nav->addCustomNav("Contact US","http://www.eclipse.org/ecf/contact.php","_self",1);
-    $Nav->addCustomNav("Contact US","contact.php","_self",1);
-    $Nav->addCustomNav("Contact US","/ecf/contact.php","_self",1);
+#   $Nav->addCustomNav("Contact US","http://www.eclipse.org/ecf/contact.php","_self",1);
+    $Nav->addCustomNav("Contact Us","contact.php","_self",1);
+#   $Nav->addCustomNav("Contact US","/ecf/contact.php","_self",1);
     
     $Nav->addCustomNav("Getting Started","/ecf/gettingStarted.php","_self",1);
     $Nav->addCustomNav("Downloads","http://www.eclipse.org/ecf/downloads.php","_self",1);
-    
-    $Nav->addCustomNav("Newsgroups","http://dev.eclipse.org/newslists/news.eclipse.technology.ecf/maillist.html","_self",1);
+    $Nav->addCustomNav("Documentation","documentation.php","_self",1);
+ 
     $Nav->addCustomNav("Bugs","http://www.eclipse.org/ecf/bugs.php","_self",1);
     $Nav->addCustomNav("Plan","http://www.eclipse.org/ecf/plans/ecf.plan.3.0.xml","_self",1);
-    $Nav->addCustomNav("Contributing","http://www.eclipse.org/ecf/dev_resources.php","_self",1);
+    $Nav->addCustomNav("Legal","legal.php","_self",1);
+     
+    $Nav->addCustomNav("Resources","http://www.eclipse.org/ecf/dev_resources.php","_self",1);
+    $Nav->addCustomNav("Newsgroups","http://dev.eclipse.org/newslists/news.eclipse.technology.ecf/maillist.html","_self",2);
     $Nav->addCustomNav("Mail Lists","https://dev.eclipse.org/mailman/listinfo/ecf-dev","_self",2);
     $Nav->addCustomNav("ECF Wiki","http://wiki.eclipse.org/Eclipse_Communication_Framework_Project","_self",2);
 
