937fa5778f51e424c81938b39db71b865c1c26aa
tkubaska <tkubaska>
2009-01-15 05:31:21 +0000

    remove duplicate build type nugget

--- downloads.php
+++ downloads.php
@@ -47,7 +47,7 @@ ob_start();
         $html = getNugget("EcfInstallViaUpdate.html",$html);
         $html = getNugget("EcfInstallViaZip.html",$html);
         $html = getNugget("EcfDailies.html",$html);
-        $html = getNugget("EcfBuildTypes.html",$html);
+       
         $html = getNugget("EcfSourceCode.html",$html);
         $html = getNugget("EcfExtraPlugins.html",$html);
         $html = getNugget("EcfBuildTypes.html",$html);
