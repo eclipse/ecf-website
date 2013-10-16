d0e01195011fa7ab54b98f0d9689442d91b448e5
slewis <slewis@composent.com>
2013-10-16 12:25:24 -0700

    Changes for ECF 3.7.0 release

--- index.php
+++ index.php
@@ -50,8 +50,8 @@
 	<div class="homeitem3col">
          <h3>Next Release</h3>
      	<div style="margin:10px;">
-     	<img border="0" src="../images/new.gif" width="31" height="14" alt="[new]"/><b>ECF 3.7 released on 10/14/2013.</b>.<br><br>
-			See also <a href="NewAndNoteworthy.html">ECF 3.7 New and Noteworthy</a>
+     	<img border="0" src="../images/new.gif" width="31" height="14" alt="[new]"/><b>ECF 3.7.0 released on 10/16/2013.</b>.<br><br>
+			See also <a href="NewAndNoteworthy.html">ECF 3.7.0 New and Noteworthy</a>
 	<div class="homeitem3col">		
  	<h3>What's New</h3>
  <table cellspacing="5" cellpadding="2" width="100%" border="0">
@@ -60,7 +60,7 @@
 			<img src="../images/Adarrow.gif" border="0" width="16" height="16" alt="[Adarrow]"/>
 		</td>
 		<td valign="top">
-	  	<b>October 14, 2013 - ECF 3.7.0 is available on eclipse.org.</b>
+	  	<b>October 16, 2013 - ECF 3.7.0 is available on eclipse.org.</b>
 			See <a href="downloads.php">here for download</a>.  See <a href="NewAndNoteworthy.html">ECF 3.7.0 New and Noteworthy</a>
            <p></p>
 		</td>
