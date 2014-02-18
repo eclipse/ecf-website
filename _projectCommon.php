7d445f18374849fee3d5e61b7ef1a4d8b7fbfd51
slewis <slewis@composent.com>
2014-02-18 10:50:29 -0800

    Changes to menu structure

--- _projectCommon.php
+++ _projectCommon.php
@@ -23,32 +23,16 @@
  	$Nav->addNavSeparator("ECF Home", "/ecf/");
     $Nav->addCustomNav("About This Project","http://www.eclipse.org/projects/project_summary.php?projectid=rt.ecf","_self",1);
     
-#   $Nav->addCustomNav("Contact US","http://www.eclipse.org/ecf/contact.php","_self",1);
     $Nav->addCustomNav("Contact Us","contact.php","_self",1);
-#   $Nav->addCustomNav("Contact US","/ecf/contact.php","_self",1);
     
-    $Nav->addCustomNav("Getting Started","/ecf/gettingStarted.php","_self",1);
     $Nav->addCustomNav("Downloads","http://www.eclipse.org/ecf/downloads.php","_self",1);
-    $Nav->addCustomNav("Documentation","documentation.php","_self",1);
+    $Nav->addCustomNav("Documentation/Wiki","http://wiki.eclipse.org/Eclipse_Communication_Framework_Project","_self",1);
  
     $Nav->addCustomNav("Bugs","http://www.eclipse.org/ecf/bugs.php","_self",1);
-    $Nav->addCustomNav("Plan","http://www.eclipse.org/ecf/plans/ecf.plan.3.0.xml","_self",1);
     $Nav->addCustomNav("Legal","legal.php","_self",1);
      
-    $Nav->addCustomNav("Resources","http://www.eclipse.org/ecf/dev_resources.php","_self",1);
-    $Nav->addCustomNav("Newsgroups","http://dev.eclipse.org/newslists/news.eclipse.technology.ecf/maillist.html","_self",2);
-    $Nav->addCustomNav("Mail Lists","https://dev.eclipse.org/mailman/listinfo/ecf-dev","_self",2);
-    $Nav->addCustomNav("ECF Wiki","http://wiki.eclipse.org/Eclipse_Communication_Framework_Project","_self",2);
+    $Nav->addCustomNav("Developer Mail List","https://dev.eclipse.org/mailman/listinfo/ecf-dev","_self",2);
+    $Nav->addCustomNav("Newsgroup","http://dev.eclipse.org/newslists/news.eclipse.technology.ecf/maillist.html","_self",2);
+    $Nav->addCustomNav("Wiki","http://wiki.eclipse.org/Eclipse_Communication_Framework_Project","_self",2);
 
-        
-    
-# 	$Nav->addCustomNav("Team", "team.php", 	"_self", 1);
-# 	$Nav->addCustomNav("Team", "team.php", 	"_self", 1);
-# 	$Nav->addCustomNav("Downloads", "downloads.php", 	"_self", 1);
-# 	$Nav->addCustomNav("Contact Us", "contact.php", 	"_self", 1);
-# 	$Nav->addCustomNav("Project Planning and Team Conference Schedule","http://wiki.eclipse.org/index.php/Eclipse_Communication_Framework_Project", "_blank", 1);
-# 	$Nav->addCustomNav("Overview Documentation", 		"documentation.php", "_self", 1);
-# 	$Nav->addCustomNav("ECF APIs (Javadocs)","org.eclipse.ecf.docs/api", "_blank", 1);
-# 	$Nav->addCustomNav("Developer Resources", "dev_resources.php", 	"_self", 1);
-# 	$Nav->addCustomNav("ECF Bugs", "https://bugs.eclipse.org/bugs/buglist.cgi?query_format=specific&order=relevance+desc&bug_status=__open__&product=ECF&content=", "_blank", 1);
 ?>
