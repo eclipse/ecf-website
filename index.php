fb260bb5538f0cd1cb6a25e3e4e5e450181821da
slewis <slewis@composent.com>
2016-08-24 19:14:06 -0700

    Changes for 3.13.2 release

--- index.php
+++ index.php
@@ -56,11 +56,21 @@
 	<div class="homeitem3col">
          <h3>Next Release</h3>
      	<div style="margin:10px;">
-     	<img border="0" src="../images/new.gif" width="31" height="14" alt="[new]"/><b>ECF Mars/3.13.1 released on 4/5/2016.</b><br><br>
+     	<img border="0" src="../images/new.gif" width="31" height="14" alt="[new]"/><b>ECF Mars/3.13.2 released on 8/25/2016.</b><br><br>
 			See also <a href="NewAndNoteworthy.html">ECF 3.13.1 New and Noteworthy</a>
 	</div>
  	<h3>What's New</h3>
  <table cellspacing="5" cellpadding="2" width="100%" border="0">
+ 	<tr>
+		<td align="left" valign="top" height="12" width="23">
+			<img src="../images/Adarrow.gif" border="0" width="16" height="16" alt="[Adarrow]"/>
+		</td>
+		<td valign="top">
+	  	<b>August 25, 2016 - ECF Mars/3.13.2 is available on eclipse.org.</b>
+			See <a href="downloads.php">here for download</a>.  The ECF 3.13.2 release is a maintenance release.  See <a href="NewAndNoteworthy.html">ECF 3.13.1 New and Noteworthy</a>
+           <p></p>
+		</td>
+	</tr>
  	<tr>
 		<td align="left" valign="top" height="12" width="23">
 			<img src="../images/Adarrow.gif" border="0" width="16" height="16" alt="[Adarrow]"/>
