3e246a7b558fa5a7531089ff9f35ac154e435195
slewis <slewis@composent.com>
2016-12-04 11:04:02 -0800

    Updates for 3.13.3 release

--- index.php
+++ index.php
@@ -56,8 +56,8 @@
 	<div class="homeitem3col">
          <h3>Next Release</h3>
      	<div style="margin:10px;">
-     	<img border="0" src="../images/new.gif" width="31" height="14" alt="[new]"/><b>ECF Mars/3.13.2 released on 8/25/2016.</b><br><br>
-			See also <a href="NewAndNoteworthy.html">ECF 3.13.1 New and Noteworthy</a>
+     	<img border="0" src="../images/new.gif" width="31" height="14" alt="[new]"/><b>ECF Mars/3.13.3 released on 12/6/2016.</b><br><br>
+			See also <a href="NewAndNoteworthy.html">ECF New and Noteworthy</a>
 	</div>
  	<h3>What's New</h3>
  <table cellspacing="5" cellpadding="2" width="100%" border="0">
