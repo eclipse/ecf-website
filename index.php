3e451e9a00340eb15fa5dfdc31ec0af65d316978
slewis <slewis@composent.com>
2019-06-24 07:38:42 -0700

    Fix for editing problem
    Change-Id: I85f315aeeea325b8be866d1eba6c8c3408741c98

--- index.php
+++ index.php
@@ -56,7 +56,7 @@
 	<div class="homeitem3col">
          <h3>Next Release</h3>
      	<div style="margin:10px;">
-     	<img border="0" src="../images/new.gif" width="31" height="14" alt="[new]"/><b>ECF 3.14.4 was released on 10/20/2018.</b><br><br>
+     	<img border="0" src="../images/new.gif" width="31" height="14" alt="[new]"/><b>ECF 3.14.5 was released on 6/20/2019.</b><br><br>
 			See also <a href="NewAndNoteworthy.html">ECF New and Noteworthy</a>
 	</div></div>
  	<h3>What's New</h3>
