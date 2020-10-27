dfee78ae791faf1f13fce232d439bd531cfe3753
slewis <slewis@composent.com>
2020-10-27 15:06:59 -0700

    Changes for 3.14.17 release
    Change-Id: I3313c722c219b3ef341cb3afa0abd4f7ec138e75

--- index.php
+++ index.php
@@ -53,14 +53,18 @@
 	   For Remote Services training, support, or expert OSGi or Eclipse development please contact <a href="mailto:slewis@composent.com">Composent.com via email</a> or bring it up
 	   on the <a href="https://dev.eclipse.org/mailman/listinfo/ecf-dev">ECF dev mailing list</a>.	
 	</div>
-	<div class="homeitem3col">
-         <h3>Next Release</h3>
-     	<div style="margin:10px;">
-     	<img border="0" src="../images/new.gif" width="31" height="14" alt="[new]"/><b>ECF 3.14.16 was released on 10/7/2020.</b><br><br>
-			See also <a href="NewAndNoteworthy.html">ECF New and Noteworthy</a>
-	</div></div>
  	<h3>What's New</h3>
  <table cellspacing="5" cellpadding="2" width="100%" border="0">
+  	<tr>
+		<td align="left" valign="top" height="12" width="23">
+			<img src="../images/Adarrow.gif" border="0" width="16" height="16" alt="[Adarrow]"/>
+		</td>
+		<td valign="top">
+	  	<b>Wednesday, October 28, 2020 - ECF 3.14.17 is available on eclipse.org.</b>
+			See <a href="downloads.php">here for download</a>.  The ECF 3.14.17 release is a bug-fix release.  See <a href="NewAndNoteworthy.html">ECF New and Noteworthy</a>
+           <p></p>
+		</td>
+	</tr>
   	<tr>
 		<td align="left" valign="top" height="12" width="23">
 			<img src="../images/Adarrow.gif" border="0" width="16" height="16" alt="[Adarrow]"/>
