626b3913e07419e36f44bf44bb978f356ef45398
slewis <slewis@composent.com>
2020-11-04 19:02:27 -0800

    Changes for release 3.14.18
    Change-Id: I9592048f1c59094652eb185c01018e68ad160df6

--- downloads.php
+++ downloads.php
@@ -30,10 +30,10 @@ ob_start();
  <div class="homeitem3col">
     <h3><?= $pageTitle ?></h3>
     <p><img border="0" src="../images/new.gif" width="31" height="14" alt="[new]"/>
-    <b>ECF 3.14.17 Now Available (10/28/2020)</b>
+    <b>ECF 3.14.18 Now Available (11/5/2020)</b>
     <p></p>
     <p>
-    ECF 3.14.17 requires <b>Eclipse 2018-06 or newer, or Apache Karaf 4.2+ with Java 8+</b>.  See <a href="http://download.eclipse.org/eclipse/downloads/">here to get
+    ECF 3.14.18 requires <b>Eclipse 2018-06 or newer, or Apache Karaf 4.2+ with Java 8+</b>.  See <a href="http://download.eclipse.org/eclipse/downloads/">here to get
     appropriate version of Eclipse</a> and <a href="http://karaf.apache.org">here for the appropriate version of Karaf</a>.  <br>
     <br>See <a href="NewAndNoteworthy.html">New and Noteworthy</a> for details of the contents of this release.<br>
  <br>See <a href="http://wiki.eclipse.org/Eclipse_Communication_Framework_Project">ECF Wiki</a> and/or the <a href="https://dev.eclipse.org/mailman/listinfo/ecf-dev">ecf dev at eclipse.org mailing list</a> for further information about plans and ongoing project activities.
@@ -44,11 +44,11 @@ ob_start();
 
         include 'getNugget.php';
 
-        $html = getNugget("EcfInstallViaKaraf3.14.17.html",$html);
+        $html = getNugget("EcfInstallViaKaraf3.14.18.html",$html);
         
-        $html = getNugget("EcfInstallViaUpdate3.14.17.html",$html);
+        $html = getNugget("EcfInstallViaUpdate3.14.18.html",$html);
         
-        $html = getNugget("EcfInstallViaZip3.14.17.html",$html);
+        $html = getNugget("EcfInstallViaZip3.14.18.html",$html);
         
         $html = getNugget("EcfDailies.html",$html);    
  
