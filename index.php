31cda4c98dbe166cccf1477b3c4e39f0eb2f96af
scottslewis <slewis@composent.com>
2022-01-06 12:26:30 -0800

    Update to ECF v3.14.30
    Change-Id: I2cfb04c3809667ec5d01c022f3c01b13d8c7fba8

--- index.php
+++ index.php
@@ -60,6 +60,16 @@
 			<img src="../images/Adarrow.gif" border="0" width="16" height="16" alt="[Adarrow]"/>
 		</td>
 		<td valign="top">
+	  	<b>Thursday, January 6, 2022 - ECF 3.14.30 is available on eclipse.org.</b>
+			See <a href="downloads.php">here for download</a>.  The ECF 3.14.30 release is a bug-fix release.  See <a href="NewAndNoteworthy.html">ECF New and Noteworthy</a>
+           <p></p>
+		</td>
+	</tr>
+  	<tr>
+		<td align="left" valign="top" height="12" width="23">
+			<img src="../images/Adarrow.gif" border="0" width="16" height="16" alt="[Adarrow]"/>
+		</td>
+		<td valign="top">
 	  	<b>Wednesday, January 5, 2022 - ECF 3.14.29 is available on eclipse.org.</b>
 			See <a href="downloads.php">here for download</a>.  The ECF 3.14.29 release is a bug-fix release.  See <a href="NewAndNoteworthy.html">ECF New and Noteworthy</a>
            <p></p>
