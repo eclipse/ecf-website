be6e85acf22df842f26319f525ea96eb828de57e
scottslewis <slewis@composent.com>
2023-05-29 15:29:21 -0700

    Update to 3.14.39 release

--- downloads.php
+++ downloads.php
@@ -30,10 +30,10 @@ ob_start();
  <div class="homeitem3col">
     <h3><?= $pageTitle ?></h3>
     <p><img border="0" src="../images/new.gif" width="31" height="14" alt="[new]"/>
-    <b>ECF 3.14.37 Now Available (11/20/2022)</b>
+    <b>ECF 3.14.39 Now Available (5/29/2029)</b>
     <p></p>
     <p>
-    ECF 3.14.37 requires <b>Eclipse 2021-6 or newer, or Apache Karaf 4.3+ with Java 8+</b>.  Note that Eclipse since 2021-03 requires Java 11 or newer, but ECF still only requires Java 8.  See <a href="http://download.eclipse.org/eclipse/downloads/">here to get
+    ECF 3.14.39 requires <b>Eclipse 2022-6 or newer, or Apache Karaf 4.3+ with Java 11+</b>.  See <a href="http://download.eclipse.org/eclipse/downloads/">here to get
     appropriate version of Eclipse</a> and <a href="http://karaf.apache.org">here for the appropriate version of Karaf</a>.  <br>
     <br>See <a href="NewAndNoteworthy.html">New and Noteworthy</a> for details of the contents of this release.<br>
  <br>See <a href="http://wiki.eclipse.org/Eclipse_Communication_Framework_Project">ECF Wiki</a> and/or the <a href="https://dev.eclipse.org/mailman/listinfo/ecf-dev">ecf dev at eclipse.org mailing list</a> for further information about plans and ongoing project activities.
@@ -44,11 +44,11 @@ ob_start();
 
         include 'getNugget.php';
 
-        $html = getNugget("EcfInstallViaKaraf3.14.37.html",$html);
+        $html = getNugget("EcfInstallViaKaraf3.14.39.html",$html);
         
-        $html = getNugget("EcfInstallViaUpdate3.14.37.html",$html);
+        $html = getNugget("EcfInstallViaUpdate3.14.39.html",$html);
         
-        $html = getNugget("EcfInstallViaZip3.14.37.html",$html);
+        $html = getNugget("EcfInstallViaZip3.14.39.html",$html);
         
         $html = getNugget("EcfDailies.html",$html);    
  
